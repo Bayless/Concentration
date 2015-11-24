f2294096ba02d68f4432f023af666e06b76c2d8c
Bayless <bsmithsalzberg@stuy.edu>
2015-11-23 20:15:40 -0500

    stuck on error

--- Concentration.java
+++ Concentration.java
@@ -1,59 +1,93 @@
+//Team Fern - Joel Ye, Bayle Smith-Salzberg
+//APCS1 pd10
+//HW36 -- Some Folks Call it a Memory
+//2015-11-23
+
 //import cs1.Keyboard;  //if comfortable with Scanner, you may comment this out
 import java.util.Scanner;
 		
 public class Concentration {
 
-		//instance variables
-		private Tile[][] _board;     //storage for 4x4 grid of Tiles.
-		private int _numberFaceUp;   //count of Tiles with faces visible
-		private String[] _words;     //list of unique Strings used for Tile vals
-		private static int _numRows = 4;
-		private gamePhase = 1;
-		private score = 1000;
+    //instance variables
+    private Tile[][] _board;     //storage for 4x4 grid of Tiles.
+    private int _numberFaceUp;   //count of Tiles with faces visible
+    private String[] _words;     //list of unique Strings used for Tile vals
+    private static int _numRows = 4;
+    private int gamePhase = 1;
+    private int score = 1000;
 
-		//insert constructor and methods here
-		_board = new Tile[4][4];
-		_words = {"a","b","c","d","e","f","g","h"};
+    //insert constructor and methods here
 
-		private static void swap( int x1, int y1, int x2, int y2 ) {
-			String temp = _board[x1][y1];
-		    	_board[x1][y1] = _board[x2][y2];
-		    	_board[x2][y2] = temp;			
-    		}
+    		
+    public Concentration(){
+	_board = new Tile[4][4];
+	_words = new String[] {"a","b","c","d","e","f","g","h"};
 
-		private static void shuffle(){
-			int len1 = _board.length;
-			int len2 = _board[0].length;
-			for (int i = 0; i < len1; i++)
-			
-				swap(i, i + (int)(Math.random()*(len-i)));
+	for (int i = 0; i < _board.length; i++){
+	    for (int j = 0; j < _board[i].length; j++){
+		_board[i][j] = _words[(i+j) % 8][(i+j)%8];
+	    }
+	    shuffle();
+	}
+    }
+
+    
+    public static String print( Tile[][] a ) {
+	String ret = "";
+	for (int i = 0;i<a.length;i++){
+	    ret+="row " + i+1 + ": \t";
+	    for (int x = 0; x < a[i].length;x++){
+		ret+=a[i][x] + " ";
+		if (x==a[i].length-1){
+		    ret+="\n";
 		}
-		}		
+	    }
+	}
+	return ret;
+    }
+    public Tile[][] getBoard(){
+	return _board;
+    }
+
+    private void swap( int x1, int y1, int x2, int y2 ) {
+	Tile temp = getBoard()[x1][y1];
+	_board[x1][y1] = getBoard()[x2][y2];
+	_board[x2][y2] = temp;			
+    }
+
+    private void shuffle(){
+	int len1 = _board.length;
+	int len2 = _board[0].length;
+	for (int i = 0; i < len1; i++){
+	    for (int x = 0; x < len2; x++){
+		swap (i,x, i + (int)(Math.random()*(len1-i)),  x + (int)(Math.random()*(len2-x)));
+	    }
+	}
+    }	
+
+
 		
-		public Concentration(){
-			for (int i = 0; i < _board.length; i++)
-				for (int j = 0; j < _board[i].length; j++)
-					_board[i][j] = _words[(i+j) % 8];
-			shuffle();
-		}
-
-		
-		public static void play(){
-			Scanner in = new Scanner(System.in);
-			while (gamePhase != 0){
+    public void play(){
+	Scanner in = new Scanner(System.in);
+	while (gamePhase != 0){
 			
 
-			}
-			System.out.println("The game is over.");
-			System.out.println("Your score is " + score + ".");	
-		}
+	}
+	System.out.println("The game is over.");
+	System.out.println("Your score is " + score + ".");	
+    }
 
 
-		//DO NOT MODIFY main()
-		public static void main(String[] args){
-				Concentration game = new Concentration();
-				game.play();
-		}
+    //DO NOT MODIFY main()
+    public static void main(String[] args){
+	Concentration test = new Concentration();
+	System.out.println(test.print(test._board));
+	test.shuffle();
+	System.out.println(test.print(test._board));
+				   
+	/*	Concentration game = new Concentration();
+		game.play(); */
+    }
 
 }//end class Concentration
 
